1f70378e9874866229ce13d6b7cd00afd7a8021a
SForkner <sforkner@juno.com>
2019-11-04 18:14:25 -0500

    C Schafer matplotlib timeseries plot

---
+++ timeseries.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+from matplotlib import dates as mpl_dates
+
+plt.style.use('seaborn')
+
+data = pd.read_csv('data5.cvs')
+
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values('Date', inplace=True)
+
+price_date = data['Date']
+price_close = data['Close']
+
+plt.plot_date(price_date, price_close, linestyle='solid')
+
+plt.gcf().autofmt_xdate()
+
+plt.title('Bitcoin Prices')
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
+
+plt.tight_layout()
+
+plt.savefig('timeseries.png')
+
+plt.show()
