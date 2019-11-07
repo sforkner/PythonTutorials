18eb427c6bfc3d581c1fb4867c34a46a8953e7e7
SForkner <sforkner@juno.com>
2019-11-07 11:44:49 -0500

    Added shell scripts to activate venv, them run the python scripts with arguments required

---
+++ pdfs/mergescript.sh
@@ -0,0 +1,5 @@
+pwd
+source venv/bin/activate
+cd pdfs
+pwd
+python3 pdfmerge.py dummy.pdf pdfno2.pdf pdfno3.pdf > pdfmerge.txt
---
+++ pdfs/watermarkscript.sh
@@ -0,0 +1,5 @@
+pwd
+source venv/bin/activate
+cd pdfs
+pwd
+python3 watermark.py dummy.pdf pdfno2.pdf pdfno3.pdf
