798935ccc250d17996297224acf6a444449ce24c
SForkner <sforkner@juno.com>
2019-11-17 11:33:36 -0500

    last

--- web server/server.py
+++ web server/server.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 # print(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
